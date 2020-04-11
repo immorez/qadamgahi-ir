056d629e0dcde9196db723ba5ffd86de84178d79
qadamgahiii <qadamgahiii@gmail.com>
2020-04-12 02:00:38 +0430

    Follow menu added to navigation bar

--- components/UI/Navigation/FollowMenu.js
+++ components/UI/Navigation/FollowMenu.js
@@ -2,9 +2,26 @@ const followMenu = () => {
   return (
     <div>
       <ul className="inline-flex list-none ml-2 transition-transform duration-500 ease-in-out">
-        <li className="mr-1">TWITTER</li>
-        <li className="mr-1">INSTAGRAM</li>
-        <li className="mr-1">TELEGRAM</li>
+        <li className="mr-2">
+          <a href="https://twitter.com/mqadamgahi">
+            <img src="/assets/images/twitter.svg" className="w-5"></img>
+          </a>
+        </li>
+        <li className="mr-2">
+          <a href="https://instagram.com/mqadamgahiii">
+            <img src="/assets/images/instagram.svg" className="w-5"></img>
+          </a>
+        </li>
+        <li className="mr-2 font-light">
+          <a href="https://t.me/qadamgahiii">
+            <img src="/assets/images/telegram.svg" className="w-5"></img>
+          </a>
+        </li>
+        <li className="mr-1">
+          <a href="https://ir.linkedin.com/in/mohammadreza-qadamgahi-b2b6b3128">
+            <img src="/assets/images/linkedin.svg" className="w-5"></img>
+          </a>
+        </li>
       </ul>
     </div>
   );
